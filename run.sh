a9a28a3bed2e8d554864e8c4e0c13cad2af560ca
FND <fnd@fnd-oda.(none)>
2010-08-19 10:23:32 +0200

    added support for multiple test files

--- run.sh
+++ run.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env sh
 
+# QUnit for Spidermonkey
+#
 # Usage:
 #   $ run.sh [-v] <testfiles>
+
+set -e
+
+die() {
+    echo "$*"
+    exit 1
+}
 
 if [ "$1" = "-v" ]; then
 	verbose=true
 	shift
 fi
 
-filename=${1:?}
+if [ -z "$1" ]; then
+	die "no test files specified"
+fi
+testfiles=""
+for filename in $@; do
+	testfiles="$testfiles -f $filename"
+done
 
 if [ -n "$verbose" ]; then
 	cfg='-e QUnit.config.headless.verbose=true;' # XXX: must not contain spaces
 fi
 
-# Spidermonkey
-js -f qunit.js -f headless.js $cfg -f $filename -f report.js
+js -f qunit.js -f headless.js $cfg $testfiles -f report.js
